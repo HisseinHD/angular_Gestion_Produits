0cc9a44b1df55fd3abd4c6bae02ca806fb75347d
HisseinHD <hisseindaoud6627@gmail.com>
2025-08-22 16:21:03 +0200

    creation du component navbar et afficher sur tt page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, Navbar],
+  template: `
+  <app-navbar></app-navbar>,
+  <router-outlet />`,
+  
   styles: ``,
 })
 export class App {
